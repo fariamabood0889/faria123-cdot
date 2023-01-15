42f41267924574410c4d7485d10975e6b68bbf2f
Faria Mabood <fariamabood0889@gmail.com>
2023-01-15 17:28:00 +0530

    added files

---
+++ diff.java
@@ -0,0 +1,17 @@
+public class DiffOfNumbers1
+{
+public static void main(String args[])
+{
+int n1 = 225, n2 = 115, Diff;
+Diff = n1 -  n2;
+System.out.println("The sum of numbers is::: "+Diff);
+}
+}
+~                                                            
+~                                                            
+~                                                            
+
+                        
+~                                                            
+~                                                            
+~     
